bb3e895fbaa38b66dbed4d37ba1d96445df177be
Sriram Thiruveedhula <sriram.thiruveedhula2007@gmail.com>
2026-05-04 02:21:52 +0530

    fix: add missing TypeScript declaration for YamlBuilder.module.scss
    Signed-off-by: Sriram Thiruveedhula <sriram.thiruveedhula2007@gmail.com>

---
+++ chaoscenter/web/src/components/YAMLBuilder/YamlBuilder.module.scss.d.ts
@@ -0,0 +1,30 @@
+declare namespace YamlBuilderModuleScssNamespace {
+  export interface IYamlBuilderModuleScss {
+    main: string;
+    borderWithPluginsPanel: string;
+    filePath: string;
+    flexCenter: string;
+    entityTag: string;
+    editor: string;
+    layout: string;
+    validationStatus: string;
+    invalidYaml: string;
+    header: string;
+    validationIcon: string;
+    darkBg: string;
+    lightBg: string;
+    errorSummary: string;
+    details: string;
+    summaryPopover: string;
+    item: string;
+    errorList: string;
+    splitPanel: string;
+    pluginDecorator: string;
+    resizeIcon: string;
+    headerBorder: string;
+  }
+}
+
+declare const YamlBuilderModuleScssModule: YamlBuilderModuleScssNamespace.IYamlBuilderModuleScss;
+
+export = YamlBuilderModuleScssModule;
